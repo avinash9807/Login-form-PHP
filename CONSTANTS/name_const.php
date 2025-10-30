e9261a00e9036652bebcce2e6cdb24b1a436fdfd
avinash9807 <82309253+avinash9807@users.noreply.github.com>
2025-10-30 12:00:07 +0530

    Add website branding and social media constants
    Defines constants for website branding and social media links.

---
+++ CONSTANTS/name_const.php
@@ -0,0 +1,41 @@
+<?php
+// name_const.php - Website Branding & Social Media Constants
+
+// Website Information
+define('SITE_NAME', 'VIP CHAUHAN');
+define('COMPANY_NAME', 'VIP CHAUHAN Studio');
+define('SITE_VERSION', '1.0');
+define('SUPPORT_EMAIL', 'support@vipbgmi.com');
+define('COPYRIGHT_YEAR', '2024');
+
+// Social Media Configuration
+define('SOCIAL_TELEGRAM', true);
+define('SOCIAL_TELEGRAM_URL', 'https://t.me/yourchannel');
+define('SOCIAL_TELEGRAM_ICON', '📱');
+define('SOCIAL_TELEGRAM_TEXT', 'Telegram');
+
+define('SOCIAL_WHATSAPP', true);
+define('SOCIAL_WHATSAPP_URL', 'https://wa.me/yournumber');
+define('SOCIAL_WHATSAPP_ICON', '💬');
+define('SOCIAL_WHATSAPP_TEXT', 'WhatsApp');
+
+define('SOCIAL_FACEBOOK', false);
+define('SOCIAL_FACEBOOK_URL', 'https://facebook.com/yourpage');
+define('SOCIAL_FACEBOOK_ICON', '📘');
+define('SOCIAL_FACEBOOK_TEXT', 'Facebook');
+
+define('SOCIAL_INSTAGRAM', true);
+define('SOCIAL_INSTAGRAM_URL', 'https://instagram.com/yourprofile');
+define('SOCIAL_INSTAGRAM_ICON', '📸');
+define('SOCIAL_INSTAGRAM_TEXT', 'Instagram');
+
+define('SOCIAL_TWITTER', true);
+define('SOCIAL_TWITTER_URL', 'https://twitter.com/yourprofile');
+define('SOCIAL_TWITTER_ICON', '🐦');
+define('SOCIAL_TWITTER_TEXT', 'Twitter');
+
+define('SOCIAL_YOUTUBE', true);
+define('SOCIAL_YOUTUBE_URL', 'https://youtube.com/yourchannel');
+define('SOCIAL_YOUTUBE_ICON', '🎥');
+define('SOCIAL_YOUTUBE_TEXT', 'YouTube');
+?>
